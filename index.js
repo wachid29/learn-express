430c025b178f619aa9d907238c170ec76618b574
wachid <wachidnurafif29@gmail.com>
2022-07-14 19:25:26 +0700

    [FIX] Ready to deploy

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-const port = process.env.PORT;
+const port = process.env.PORT || 8001;
 const bodyParser = require("body-parser");
 const helmet = require("helmet");
 const cors = require("cors");
